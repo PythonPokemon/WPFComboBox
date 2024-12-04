57e29ce518ad0fbecb1cdd6a4de4ccdacc6b39b9
jakob.derzapf <Jakob.D.Baron@outlook.de>
2024-12-04 13:03:41 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFComboBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            KomboBoxFarben.ItemsSource = typeof(Colors).GetProperties(); // alle farben die in 'Colors' drinn sind setz man durch Itemsource in 'Kombofarben' rein
+
+        }
+    }
+}
